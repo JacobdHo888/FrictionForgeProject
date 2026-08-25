00928a60e69f90a01d7a3443620dc5db7d0b6d9c
JacobdHo <148382652+JacobdHo888@users.noreply.github.com>
2026-08-24 22:56:03 -0700

    Altered the UI to appear more vibrant and adjusted the input system.

--- frontend/vertex-ai-proxy-interceptor.js
+++ frontend/vertex-ai-proxy-interceptor.js
@@ -113,7 +113,7 @@
           headers: {
             'Content-Type': 'application/json',
             // Add a random header to identify these proxied requests on the Node.js backend.
-            'X-App-Proxy': 'mp2J8UlxLz_ohHQRVcxeTEDCa4MtuP4o',
+            'X-App-Proxy': 'uCFCvn3n9HuPhaYSLxy4ioYsTLZaK9nt',
           },
           body: JSON.stringify(requestDetails),
         };
